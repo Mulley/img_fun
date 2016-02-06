1d55fc1957b813522cf9f078a47599a971614ee8
Mulley <mullen.michael523@gmail.com>
2016-02-06 15:27:14 -0800

    Added known_colors.py with primary and secondary colors

---
+++ known_colors.py
@@ -0,0 +1,9 @@
+"""Colors for the distance alteration."""
+# Primary Colors
+RED = {'r': 255, 'g': 0, 'b': 0}
+BLUE = {'r': 0, 'g': 0, 'b': 255}
+YELLOW = {'r': 255, 'g': 255, 'b': 0}
+# Secondary Colors
+ORANGE = {'r': 255, 'g': 165, 'b': 0}
+GREEN = {'r': 0, 'g': 255, 'b': 0}
+PURPLE = {'r': 155, 'g': 48, 'b': 255}
