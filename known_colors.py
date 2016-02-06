417013539387f505c47d86e622de3e4d16906aeb
Mulley <mullen.michael523@gmail.com>
2016-02-06 15:58:07 -0800

    Added colors, get_numpy_list

--- known_colors.py
+++ known_colors.py
@@ -1,4 +1,5 @@
 """Colors for the distance alteration."""
+import numpy as np
 # Primary Colors
 RED = {'r': 255, 'g': 0, 'b': 0}
 BLUE = {'r': 0, 'g': 0, 'b': 255}
@@ -7,3 +8,21 @@ YELLOW = {'r': 255, 'g': 255, 'b': 0}
 ORANGE = {'r': 255, 'g': 165, 'b': 0}
 GREEN = {'r': 0, 'g': 255, 'b': 0}
 PURPLE = {'r': 155, 'g': 48, 'b': 255}
+# Other Colors
+BLACK = {'r': 0, 'g': 0, 'b': 0}
+GRAY = {'r': 40, 'g': 40, 'b': 40}
+LIGHT_GRAY = {'r': 190, 'g': 190, 'b': 190}
+WHITE = {'r': 255, 'g': 255, 'b': 255}
+
+
+COMPARISON_LIST = [RED, BLUE, YELLOW, ORANGE, GREEN, PURPLE,
+                   BLACK, GRAY, LIGHT_GRAY, WHITE]
+
+
+def get_numpy_list(colors=COMPARISON_LIST):
+    """Return COMPARISON_LIST as a list of numpy arrays."""
+    np_list = []
+    for color in colors:
+        np_list.append(np.array((color['r'], color['g'], color['b'])))
+
+    return np_list
